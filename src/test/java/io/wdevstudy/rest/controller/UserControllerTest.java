02d210a3704a4256cad09944aaa63658949bce87
우연 <68698090+Fiadh-L@users.noreply.github.com>
2023-01-29 18:17:11 +0900

    upload project wdevstudy_230129

---
+++ src/test/java/io/wdevstudy/rest/controller/UserControllerTest.java
@@ -0,0 +1,40 @@
+package io.wdevstudy.rest.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    private MockMvc 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void modify() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
